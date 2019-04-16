3cc5a6d2bdbf08a68cdfc065ba7034b3ad4372de
silpa016 <43297600+silpa016@users.noreply.github.com>
2019-04-16 09:06:37 +0530

    Add files via upload

---
+++ Assignment1_prog3.py
@@ -0,0 +1,6 @@
+first_name=input("enter first name")
+last_name= input("enter last name")
+
+print(last_name +" "+ first_name)
+print(last_name[::-1]+" "+first_name[::-1])
+
---
+++ Assignment1_prog4.py
@@ -0,0 +1,6 @@
+import math
+pi = math.pi
+r = 15
+v=(4/3)*(pi)*r**3
+print(v)
+
